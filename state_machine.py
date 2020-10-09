f8f91d2d9bd6615e7c926ef42f8d481e297253c3
Jan <j.c.schagen@student.utwente.nl>
2020-10-09 11:55:04 +0200

    Update state_machine.py

---
+++ state_machine.py
@@ -0,0 +1,27 @@
+
+#All states
+
+#Start
+    #Calibrating the EMG
+    #Set motors to location 0
+
+#Receive
+    #Read and filter EMG1
+    #Read and filter EMG2
+    #Read and filter EMG3
+
+#Process
+    #Using EMG1 set biceps1 to 0 or 1
+    #Using EMG2 set biceps2 to 0 or 1
+    #Using EMG3 set calfmuscle to 0 or 1
+    
+    #Turn these three states into the commands:
+        #rightclick
+        #leftclick
+        #up
+        #down
+        #left
+        #right
+
+#move
+    #Read the received commands from process and move the right motor accordingly
