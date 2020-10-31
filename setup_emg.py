dac896ba46a70cf53999735448d0a9cdf54673b0
Jan <j.c.schagen@student.utwente.nl>
2020-10-31 15:26:22 +0100

    Update setup_emg.py

--- setup_emg.py
+++ setup_emg.py
@@ -4,16 +4,13 @@ from machine import Pin, ADC
 from emg_read import EmgReader
 
 class EmgRun(object):
-    def __init__(self, readtime, ticker):
-        self.ticker_number = ticker
+    def __init__(self, readtime, reader_number):
+        self.ticker_number = 15
+        self.reader_number = reader_number
 
-        self.EmgReader1 = EmgReader(1)
-        self.EmgReader2 = EmgReader(2)
-        self.EmgReader3 = EmgReader(3)
+        self.EmgReader = EmgReader(self.reader_number)
 
         self.dataset1 = [] #set up empty dataset to start with
-        self.dataset2 = []
-        self.dataset3 = []
 
         self.done = False
 
@@ -31,15 +28,9 @@ class EmgRun(object):
 
     def loop(self):
         if self.count < (self.main_frequency*self.readtime):
-            a = self.EmgReader1.read_emg()
+            a = self.EmgReader.read_emg()
             #print("Emg1 ",a)
             self.dataset1.append(a)
-            b = self.EmgReader2.read_emg()
-            #print("Emg2 ",b)
-            self.dataset2.append(b)
-            c = self.EmgReader3.read_emg()
-            #print("Emg3 ", c)
-            self.dataset3.append(c)
             self.count += 1
             #print(self.count)
             self.done = False
@@ -50,14 +41,8 @@ class EmgRun(object):
             self.done = True
         return
     
-    def datareceive1(self):
+    def datareceive(self):
         return(self.dataset1)
-
-    def datareceive2(self):
-        return(self.dataset2)
-    
-    def datareceive3(self):
-        return(self.dataset3)
 
     def checkdone(self):
         return(self.done)
